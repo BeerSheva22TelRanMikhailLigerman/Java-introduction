9bd4a3cf0f00004ed22b96333bb5f8cfe51ebc84
user <user@192.168.1.194>
2022-11-23 21:37:14 +0200

    HW10 22.11.2022

--- src/ObjectTests.java
+++ src/ObjectTests.java
@@ -30,6 +30,7 @@ class ObjectTests {
 	}
 	
 	@Test
+	@Disabled
 	void isAnagramTest() {
 		assertTrue(Strings.isAnagram("qweasd", "asdqwe"));
 		assertTrue(Strings.isAnagram("qqqwww", "wwwqqq"));
@@ -43,11 +44,54 @@ class ObjectTests {
 	}
 	
 	@Test
+	@Disabled
 	void sortStringNumbersTest() {
 		String[] array1 = { "5", "-37", "15", "2", "22", "120", "118", "-110", "5", "2", "1", "0", "127", "-128", "-128", "1", "5" };
 		Strings.sortStringNumbers(array1);
 		assertArrayEquals(new String[] { "-128", "-128", "-110", "-37", "0", "1", "1", "2", "2", "5", "5", "5", "15", "22", "118", "120", "127" }, array1);
 	}
+	
+	@Test
+	@Disabled
+	void javaVariableTest() {
+		assertTrue("java".matches(Strings.javaNameExp()));
+		assertTrue("__".matches(Strings.javaNameExp()));
+		assertTrue("java_1_2".matches(Strings.javaNameExp()));
+		assertTrue("__".matches(Strings.javaNameExp()));
+		assertFalse("1java".matches(Strings.javaNameExp()));
+		assertFalse("_".matches(Strings.javaNameExp()));
+		assertFalse("$ _".matches(Strings.javaNameExp()));
+	}
+	
+	@Test
+	
+	void ipV4OctetTest() {
+		assertTrue("0".matches(Strings.ipV4Octet()));
+		assertTrue("1".matches(Strings.ipV4Octet()));
+		assertTrue("00".matches(Strings.ipV4Octet()));
+		assertTrue("99".matches(Strings.ipV4Octet()));
+		assertTrue("199".matches(Strings.ipV4Octet()));
+		assertTrue("249".matches(Strings.ipV4Octet()));
+		assertTrue("255".matches(Strings.ipV4Octet()));
+		assertFalse(" ".matches(Strings.ipV4Octet()));
+		assertFalse("256".matches(Strings.ipV4Octet()));
+		assertFalse("311".matches(Strings.ipV4Octet()));
+		assertFalse("1O0".matches(Strings.ipV4Octet()));
+	}
+	
+	@Test
+	
+	void ipV4Test() {
+		assertTrue("0.0.0.0".matches(Strings.ipV4()));
+		assertTrue("192.168.0.1".matches(Strings.ipV4()));
+		assertTrue("255.255.255.255".matches(Strings.ipV4()));
+		assertFalse("0.0.0.256".matches(Strings.ipV4()));
+		assertFalse("0.0.0,0".matches(Strings.ipV4()));
+		assertFalse("0.0.0.O".matches(Strings.ipV4()));
+		assertFalse("0.0.0.0.".matches(Strings.ipV4()));
+	}
+		
+	
 	
 
 }
